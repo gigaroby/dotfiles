e79551deb66b917fa8dea4da97dfb14b26d9d8b3
Roberto Bampi <bampi.roberto@gmail.com>
2012-12-25 20:23:29 +0100

    fixed setup file

--- setup.sh
+++ setup.sh
@@ -4,17 +4,13 @@
 function link_file {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
-    if [ -e "${target}" ]; then
-        mv $target $target.bak
+    if [ -e "${target}" ] && [ ! -L "${target}" ]; then
+        mv $target $target.df.bak
     fi
         ln -sf ${source} ${target}
 }
 
-link_file vimrc
+link_file _vimrc
 
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
+git submodule update --init --recursive
+git submodule foreach --recursive git pull origin master
