8a24e09d040610daf272e64ad77adaf3f56539e2
Roberto Bampi <bampi.roberto@gmail.com>
2013-08-20 15:17:16 +0200

    zsh configuration addedd

--- setup.sh
+++ setup.sh
@@ -18,5 +18,12 @@ git clone https://github.com/gmarik/vundle.git $dotvim/bundle/vundle
 link_file _vimrc
 vim +BundleInstall +qall
 
-#Tmux config
+# Tmux config
 link_file _tmux.conf
+
+# Oh-my-zsh
+rm -rf $HOME/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+link_file _zshrc
+
+
