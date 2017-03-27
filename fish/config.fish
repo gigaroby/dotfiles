2a0d47e95115a4f9d91fc988a4fde3fddd227e40
Roberto Bampi <bampi.roberto@gmail.com>
2017-03-27 10:46:59 +0200

    fish: detect os and export stuff for mac only

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH /usr/local/sbin
-set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+switch (uname)
+    case Darwin
+        set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH /usr/local/sbin
+        set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+    case Linux
+end
 
 set -x GOPATH $HOME/projects/go
 set -x PATH $PATH $HOME/.go/bin $GOPATH/bin
